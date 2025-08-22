10ac3f486e9486f83ae3775cf94dc2e8496ca93c
Prof. Me. Ricardo de Andrade Kratz <rakratz@gmail.com>
2025-08-22 09:59:19 -0300

    Update VetorNaoOrdenado.c

--- Scripts/Aula04/VetorNaoOrdenado.c
+++ Scripts/Aula04/VetorNaoOrdenado.c
@@ -21,6 +21,31 @@ int insere_final(Lista* lista, int elem) {
     return 1; // sucesso
 }
 
+int busca_elem(const Lista* lista, int elem){
+    if (lista == NULL) return -1;
+    for (int i =0; i < lista->qtd; i++){
+        if (lista->dados[i] == elem) return i;
+    }
+}
+
+int remove_elem(Lista* lista, int elem) {
+    if (lista == NULL || lista->qtd == 0) return 0;  // lista vazia ou nula
+
+    int i = 0;
+    while (i < lista->qtd && lista->dados[i] != elem) i++;  // busca linear
+
+    if (i == lista->qtd) return 0;  // não encontrou
+
+    // desloca à esquerda do índice encontrado até o penúltimo
+    for (int j = i; j < lista->qtd - 1; j++) {
+        lista->dados[j] = lista->dados[j + 1];
+    }
+
+    lista->qtd--;   // diminui o tamanho lógico
+    return 1;   // sucesso
+}
+
+
 void imprime_lista(const Lista* lista) {
     if (lista == NULL) return;
     printf("Lista: ");
@@ -32,17 +57,18 @@ void imprime_lista(const Lista* lista) {
 
 int main() {
     Lista* lista = cria_lista();
-    if (!lista) {
-        fprintf(stderr, "Falha ao alocar memória.\n");
-        return 1;
-    }
 
     insere_final(lista, 10);
     insere_final(lista, 20);
+    insere_final(lista, 10); // valor repetido
     insere_final(lista, 30);
-    insere_final(lista, 40);
+    imprime_lista(lista); // Lista: 10 20 10 30
 
-    imprime_lista(lista);
+    int pos = busca_elem(lista, 10);
+    if (pos != -1) printf("Elemento 10 encontrado na posicao %d\n", pos);
+
+    remove_elem(lista, 10); // remove a primeira ocorrência
+    imprime_lista(lista);   // Lista: 20 10 30
 
     free(lista);
     return 0;
