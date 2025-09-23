6e52130fa89f58a454ef8bf85a3c63983027ecc8
Prof. Me. Ricardo de Andrade Kratz <rakratz@gmail.com>
2025-09-23 09:08:00 -0300

    Add files via upload

---
+++ Aula 14/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lista.h"
+
+int main()
+{
+    Lista L;
+    inicializaLista(&L);
+
+    // Verificar se existe a lista e está vazia
+    printf("A lista existe e esta vazia? \n");
+    if (estaVazia){
+        printf("Sim e ");
+    }
+    imprimeLista(&L);
+    return 0;
+}
